2b3c9b14c1d9a2cb0a6dfac6b58d4dfbe0022c9f
Xavier Jalencas <xavier.jalencas@gmail.com>
2026-02-05 15:23:15 +0100

    Python Session 08 Examples

---
+++ PYT.S09/PYTS08.py
@@ -0,0 +1,96 @@
+class Subject:
+
+    def __init__(self, name, hours, teacher):
+
+        self.name = name
+        self.hours = hours
+        self.teacher = teacher
+
+        self.students = set()
+
+    def add_student(self, student):
+        self.students.add(student)
+
+
+class Person:
+
+    def __init__(self, name, surname, NIE, birth_date=None):
+
+        self.name = name
+        self.surname = surname
+        self.NIE = NIE
+        self.birth_date = birth_date
+
+    def __repr__(self):
+        return f"Person({self.name.__repr__()}, {self.surname!r}, {self.NIE!r})"
+
+    def __str__(self):
+        return "I am a Person object instance. My name is %s %s and my NIE is %s" %(self.name, self.surname, self.NIE)
+
+    def __eq__(self, other_person):
+        return self.NIE == other_person.NIE
+
+    def __lt__(self, other):
+        return self.surname < other.surname
+
+    def __le__(self, other):
+        return self.surname <= other.surname
+
+    def __gt__(self, other):
+        return self.surname > other.surname
+
+    def __ge__(self, other):
+        return self.surname >= other.surname
+
+    def __hash__(self):
+        return self.NIE.__hash__()
+
+    def __add__ (self, other):
+        return type(self)(self.name+other.name, self.surname, "uxxxx", "None")
+
+
+class Teacher(Person):
+
+    def __init__(self, name, surname, NIE, birth_date, salary):
+
+        self.salary = salary
+        super().__init__(name=name,
+                         surname=surname,
+                         NIE=NIE,
+                         birth_date=birth_date)
+
+
+class Student(Person):
+    pass
+
+
+student_instance1 = Student(name="Pere",
+                            surname="Anton",
+                            birth_date="1/1/1990",
+                            NIE="u5241")
+
+student_instance2 = Student(name="Pere",
+                            surname="Anton",
+                            birth_date="1/1/1990",
+                            NIE="u5241")
+
+student_instance3 = Student("Nuria", "Gonzalez", "u22312", "20/10/1992")
+student_instance4 = Student("Toni", "Bonet", "u22312", "20/1071992")
+
+my_student_list = [student_instance1,
+                   student_instance2,
+                   student_instance3,
+                   student_instance4]
+
+teacher_instance = Teacher(name="Xavi", surname="Jalencas", NIE="u1234",
+                          birth_date="1/1/1980", salary=100)
+
+PYT = Subject(name="PYT", hours=125, teacher=teacher_instance)
+
+PYT.add_student(student_instance1)
+PYT.add_student(student_instance2)
+PYT.add_student(student_instance3)
+PYT.add_student(student_instance4)
+PYT.add_student(student_instance1)
+
+
